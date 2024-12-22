35c5eb0686817ad1ea1832fbe4866a79863053e0
Alexey <codenamed.asyncz@gmail.com>
2024-12-22 22:24:05 +0300

    Design development pt.1

--- rts/users/models.py
+++ rts/users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
-
 class User(AbstractUser):
     ROLE_CHOICES = [
         ('user', 'User'),
